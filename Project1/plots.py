6208e43c23b3bd715d2e58687a2688e6b37eb969
Kristoffer Langstad <54596994+krilangs@users.noreply.github.com>
2019-09-20 19:32:44 +0200

    Update plots.py

--- Project1/plots.py
+++ Project1/plots.py
@@ -6,7 +6,9 @@ from mpl_toolkits.mplot3d import Axes3D
 from imageio import imread
 import matplotlib.pyplot as plt
 from matplotlib import cm
+import seaborn as sns
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
 
 def plot3d(x, y, z, z2):
     """
@@ -42,12 +44,36 @@ def terrain():
     Function that loads the terrain data of Hardangervidda, and shows it.
     """
     # Load the terrain
-    terrain1 = imread("n59_e010_1arc_v3.tif")
+    vidda = imread("n59_e010_1arc_v3.tif")
     #print(terrain1.shape)
     # Show the terrain
     plt.figure()
     plt.title("Terrain over Hardangervidda")
-    plt.imshow(terrain1, cmap="gray")
+    plt.imshow(vidda, cmap="gray")
     plt.xlabel("X")
     plt.ylabel("Y")
+    
+    # Show the terrain
+    sns.set()
+    sns.set_style("whitegrid")
+    sns.set_palette("husl")
+    
+    fig = plt.figure()
+    ax = fig.gca(projection="3d")
+    
+    #get the number of points
+    n_y, n_x = np.shape(vidda)
+    
+    #making an x and y grid
+    x_grid, y_grid = np.meshgrid(np.arange(n_x),np.arange(n_y))
+    
+    #print(np.shape(vidda))
+    #print(np.shape(x_grid))
+    
+    # Plot the surface.
+    surf = ax.plot_surface(x_grid, y_grid, vidda, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=False, alpha = 0.5)
+    
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.show()
