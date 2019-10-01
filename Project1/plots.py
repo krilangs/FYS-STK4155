29c635a5744824c834b71c9c4473a1e41745477c
Kristoffer Langstad <54596994+krilangs@users.noreply.github.com>
2019-10-01 12:51:14 +0200

    Update plots.py

--- Project1/plots.py
+++ Project1/plots.py
@@ -14,9 +14,11 @@ def plot3d(x, y, z, z2):
     """
     Function to make a 3d plot of the Franke function by using OLS.
     """
+    path = "figures/"
+    
     fig = plt.figure()
     ax = fig.gca(projection="3d")
-    #ax = fig.add_subplot(121, projection = "3d")
+    ax.text2D(0.05, 0.95, "3D plot of FrankeFunction", transform=ax.transAxes, size=16)
     # Plot the surface.
     surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
     # Customize the z axis.
@@ -25,10 +27,13 @@ def plot3d(x, y, z, z2):
     ax.zaxis.set_major_formatter(FormatStrFormatter("%.02f"))
     # Add a color bar which maps values to colors
     fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.tight_layout()
+    plt.savefig(path + "Franke_3Dplot" + ".png")
 
     fig = plt.figure()
-    #ax = fig.add_subplot(122, projection = "3d")
     ax = fig.gca(projection="3d")
+    ax.text2D(0.05, 0.95, "3D plot of FrankeFunction with noise",
+              transform=ax.transAxes, size=16)
     # Plot the surface.
     surf = ax.plot_surface(x, y, z2, cmap=cm.coolwarm, linewidth=0, antialiased=False)
     # Customize the z axis.
@@ -37,6 +42,53 @@ def plot3d(x, y, z, z2):
     ax.zaxis.set_major_formatter(FormatStrFormatter("%.02f"))
     # Add a color bar which maps values to colors.
     fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.tight_layout()
+    plt.savefig(path + "Franke_noise_3Dplot" + ".png")
+    plt.show()
+
+def plot_conf_int(N, method=""):
+    """
+    Function to plot the confidence interval of beta.
+    """
+    from proj1 import FrankeFunction, DesignMatrix, confidence_int, OLS
+    fsize = 10 			# universal fontsize for plots
+    path = "figures/"
+
+    x = np.sort(np.random.uniform(0, 1, N))
+    y = np.sort(np.random.uniform(0, 1, N))
+    x, y = np.meshgrid(x, y)
+    Z = np.ravel(FrankeFunction(x, y))
+    Zn = Z + 0.8*np.random.normal(loc=0.0, scale=1, size=N*N)
+    x = np.ravel(x)
+    y = np.ravel(y)
+    M = DesignMatrix(x, y, n=5)
+
+    if method == "OLS":
+        betaSTD_f = confidence_int(x, y, Z, method)
+        beta_f = OLS(M, Z)
+        betaSTD_z = confidence_int(x, y, Zn, method)
+        beta_z = OLS(M, Zn)
+        N = len(betaSTD_z)
+
+    colors = ["mediumblue","crimson"]
+    plt.plot(-10, -1, color=colors[0], label="without noise")
+    plt.plot(-10, -1, color=colors[1], label="with noise")
+    plt.legend()
+    
+    for i in range(N):
+        plt.errorbar(i, beta_f[i], yerr=betaSTD_f[i], capsize=4, \
+			color=colors[0], marker='.', markersize=7, elinewidth=2,\
+			alpha=0.5)
+        plt.errorbar(i, beta_z[i], yerr=betaSTD_z[i], capsize=4, \
+			color=colors[1], marker='.', markersize=7, elinewidth=2,\
+			alpha=0.5)
+    xticks = [r'$\beta_{%d}$'%i for i in range(N)]
+    plt.xticks(range(N), xticks, fontsize=fsize)
+    plt.xlim(-1, N)
+    plt.title("Confidence interval of $\\beta$ for " + str(method))
+    plt.tight_layout()
+    plt.savefig(path + "confIntBeta_" + str(method) + ".png")
+    plt.grid("on")
     plt.show()
 
 def terrain():
@@ -77,3 +129,4 @@ def terrain():
     # Add a color bar which maps values to colors.
     fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.show()
+#terrain()
